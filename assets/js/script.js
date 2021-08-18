dfb4989506acd2b400c303b5d460eed045849c40
Jeremiah <jayteemaubane@gmail.com>
2021-08-18 15:33:48 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,7 @@ var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 
 button.addEventListener('click',function(){
-	fetch('http://api.openweathermap.org/data/2.5/forecast?id='+inputValue.value+'&appid=a42df7c63904861c13900df4c12bdeac')
-	.then(response => response.json())
-	.then(data => {
+	fetch('http://api.openweathermap.org/data/2.5/forecast?id='+inputValue.value+'&appid=a42df7c63904861c13900df4c12bdeac').then(response => response.json()).then(data => {
 		var nameValue = data['name'];
 		var tempValue = data['main']['temp'];
 		var descValue = data['weather'][0]['description'];
