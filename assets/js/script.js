7ad28095a7fbb15e56d7bce118484b8836af277f
Jeremiah <jayteemaubane@gmail.com>
2021-08-18 15:05:46 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
-var name = document.querySelector('.name')
-var desc = document.querySelector('.desc')
-var temp = document.querySelector('.temp')
+var button = document.querySelector('.button');
+var inputValue = document.querySelector('.inputValue');
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
 
 button.addEventListener('click', function(){
 	fetch('http://api.openweathermap.org/data/2.5/forecast?id='+inputValue.value+'&appid=a42df7c63904861c13900df4c12bdeac')
